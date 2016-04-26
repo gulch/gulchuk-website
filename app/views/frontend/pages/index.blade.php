152b8f7c8be286cf35f9f6291c2062cac9add7fc
gulch <contact@gulch.in.ua>
2016-04-26 15:34:00 +0300

    add tests edit cv page

---
+++ app/views/frontend/pages/index.blade.php
@@ -0,0 +1,27 @@
+@extends('frontend.template')
+
+@section('content')
+    <main class="center-flex">
+        <section class="section vcenter-section">
+            <div class="container">
+                <div class="columns is-vcentered is-centered">
+                    <div class="column is-9">
+                        <div class="box">
+                            <h1 class="title is-2">Hello!</h1>
+                            <p class="welcome-text">
+                                My name is Volodymyr Gulchuk. I'm web developer. As a hobby I investigate web
+                                performance optimization.
+                                I write to my <a href="https://gulch.in.ua">ukrainian language blog</a> and
+                                sometimes <a href="/blog">articles in english</a>, develop <a
+                                        href="https://github.com/gulch">composer packages</a>
+                                and contribute to another open-source projects, love photography, cycling, and doing
+                                other awesome things.
+                                I live in Kyiv, Ukraine.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+@endsection
