6910054fc345ad0072fc97f07fd0669a85fbbcba
gulch <gulch958089>
2017-10-06 20:02:49 +0300

    small fixes

--- resources/views/frontend/includes/footer.php
+++ resources/views/frontend/includes/footer.php
@@ -1,9 +1,5 @@
 <footer>
     <div class="footer-content">
-        <a href="https://gulch.in.ua" class="item" target="_blank" rel="noopener">GULCH.IN.UA</a>
-        &nbsp;
-        &#8226;
-        &nbsp;
-        <a href="https://funtime.kiev.ua" class="item" target="_blank" rel="noopener">FUNTIME KIEV</a>
+        <a href="https://itldc.com/?from=4938" class="item" target="_blank" rel="noopener nofollow">AWESOME SERVERS</a>
     </div>
 </footer>
