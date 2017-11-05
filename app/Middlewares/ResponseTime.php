6dc8189d10466ea0bfcf4b7aabe88ca815c847ba
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2017-11-05 16:13:00 +0200

    improved ResponseTime middleware

--- app/Middlewares/ResponseTime.php
+++ app/Middlewares/ResponseTime.php
@@ -7,12 +7,20 @@ use Psr\Http\Message\ResponseInterface as Response;
 
 class ResponseTime
 {
+    const HEADER = 'X-Response-Time';
+
     public function __invoke(Request $request, Response $response, callable $next = null): Response
     {
+        $server = $request->getServerParams();
+
+        if (!isset($server['REQUEST_TIME_FLOAT'])) {
+            $server['REQUEST_TIME_FLOAT'] = microtime(true);
+        }
+
         $response = $next($request, $response);
 
-        $duration = round(microtime(true) * 1000) - round(APP_START * 1000);
+        $duration = (microtime(true) - $server['REQUEST_TIME_FLOAT']) * 1000;
 
-        return $response->withHeader('X-Response-Time', sprintf('%dms', $duration));
+        return $response->withHeader(self::HEADER, sprintf('%2.3f ms', $duration));
     }
 }
--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -1,7 +1,5 @@
 <?php
 
-define('APP_START', microtime(true));
-
 // error_reporting(E_ALL);
 // ini_set('display_errors', 1);
 
