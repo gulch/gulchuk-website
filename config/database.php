f00197a7421d53c9d7f4be31cfe959710ca45816
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2018-12-08 19:54:10 +0200

    small configuration changes

--- config/database.php
+++ config/database.php
@@ -7,7 +7,7 @@ return [
     'username'  => env('DB_USER'),
     'password'  => env('DB_PASS'),
     'charset'   => 'utf8mb4',
-    'collation' => 'utf8mb4_unicode_ci',
+    'collation' => 'utf8mb4_general_ci',
     'prefix'    => '',
     'strict'    => false,
 ];
--- config/jobs.php
+++ config/jobs.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'php-fpm_socket_path' => env('PHP_FPM_SOCKET_PATH', '/var/run/php/php7.2-fpm.sock'),
+    'php-fpm_socket_path'            => env('PHP_FPM_SOCKET_PATH', '/var/run/php/php7.3-fpm.sock'),
     'map' => [
         'CreateWebp'                 => '\App\Jobs\CreateWebpJob::handle',
         'CreateArticleSocialImage'   => '\App\Jobs\CreateArticleSocialImageJob::handle',
