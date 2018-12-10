39d663245d0488499b9b0ddb7f08ce7ef1bb9607
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2018-12-10 15:49:31 +0200

    removed Eloquent models, Eloquent package

--- config/queue.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'name' => 'gulchuk.com:queue',
-    'map'  => [
-        'CreateWebp'               => '\App\Jobs\CreateWebpJob::handle',
-        'CreateArticleSocialImage' => '\App\Jobs\CreateArticleSocialImageJob::handle',
-    ]
-];
