121ab3275adfda828047b56ed686859df8c427a6
Volodymyr Gulchuk <volodymyr@gulchuk.com>
2018-12-11 16:49:30 +0200

    semantic ui project overrides scripts

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,14 @@ var nano = require('gulp-cssnano');
 var discard_comments = require('postcss-discard-comments');
 var combine_duplicated_selectors = require('postcss-combine-duplicated-selectors');
 var sorting = require('postcss-sorting');
+var less = require('gulp-less');
 
 var APP_VERSION = '1.4.0';
 var BUILD_PATH = 'public/build/' + APP_VERSION;
 var VENDOR_ASSETS_PATH = 'public/assets/vendor/';
 var JS_ASSETS_PATH = 'public/assets/js/';
 var CSS_ASSETS_PATH = 'public/assets/css/';
+var LESS_ASSETS_PATH = 'public/assets/less/';
 var SEMANTIC_VERSION = '2.3.1';
 var SEMANTIC_PATH = VENDOR_ASSETS_PATH + 'semantic/' + SEMANTIC_VERSION + '/components/';
 var PRISM_VERSION = '1.6.0';
@@ -41,23 +43,39 @@ var fontsCss = function () {
         .pipe(gulp.dest(BUILD_PATH));
 };
 
-var frontendCss = function () {
+var old__frontendCss = function () {
     return gulp.src([
         SEMANTIC_PATH + 'reset.css',
         SEMANTIC_PATH + 'site.css',
         SEMANTIC_PATH + 'container.css',
-        SEMANTIC_PATH + 'grid.css',
-        SEMANTIC_PATH + 'segment.css',
-        SEMANTIC_PATH + 'image.css',
-        SEMANTIC_PATH + 'site.css',
-        SEMANTIC_PATH + 'menu.css',
-        SEMANTIC_PATH + 'header.css',
         SEMANTIC_PATH + 'divider.css',
+        SEMANTIC_PATH + 'header.css',
+        SEMANTIC_PATH + 'image.css',
         SEMANTIC_PATH + 'list.css',
+        SEMANTIC_PATH + 'segment.css',
         SEMANTIC_PATH + 'breadcrumb.css',
+        SEMANTIC_PATH + 'grid.css',
+        SEMANTIC_PATH + 'menu.css',
         CSS_ASSETS_PATH + 'frontend.css'
     ])
         .pipe(concat('f.css'))
+        .pipe(postcss([
+            discard_comments({removeAll: true}),
+            combine_duplicated_selectors(),
+            autoprefixer(),
+            sorting()
+        ]))
+        .pipe(nano())
+        .pipe(gulp.dest(BUILD_PATH));
+};
+
+var frontendCss = function () {
+    return gulp.src([
+        LESS_ASSETS_PATH + 'overrides/semantic/semantic.less',
+        CSS_ASSETS_PATH + 'frontend.css'
+    ])
+        .pipe(concat('f.css'))
+        .pipe(less())
         .pipe(postcss([
             discard_comments({removeAll: true}),
             combine_duplicated_selectors(),
